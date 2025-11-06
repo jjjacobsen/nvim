084e9288970ff55d08e1b5f82a0a147aed32501c
Jonah Jacobsen <jjacobsen115@gmail.com>
2025-11-06 13:47:38 -0600

    Enable syntax highlighting and indentation on languages

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,13 +1,18 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  lazy = false,
-  branch = "main",
-  build = ":TSUpdate",
-  config = function()
-    local ts = require("nvim-treesitter")
-    ts.setup({
-      install_dir = vim.fn.stdpath("data") .. "/site",
-    })
-    ts.install({ "rust", "javascript", "zig", "python", "javascript", "bash", "lua" })
-  end,
+	"nvim-treesitter/nvim-treesitter",
+	lazy = false,
+	branch = "main",
+	build = ":TSUpdate",
+	config = function()
+		local ts = require("nvim-treesitter")
+		local languages = { "bash", "javascript", "lua", "python", "rust", "zig" }
+		ts.setup({ install_dir = vim.fn.stdpath("data") .. "/site" })
+		ts.install(languages)
+		vim.api.nvim_create_autocmd("FileType", {
+			callback = function(args)
+				pcall(vim.treesitter.start, args.buf)
+				vim.bo[args.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+			end,
+		})
+	end,
 }
