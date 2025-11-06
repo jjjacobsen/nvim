ed502f0f398b2138c374fd6b2b52e0d3d7d4cb4a
Jonah Jacobsen <jjacobsen115@gmail.com>
2025-11-06 15:48:29 -0600

    Add fzf to telescope for better performance

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,12 +1,27 @@
 return {
 	"nvim-telescope/telescope.nvim",
 	tag = "0.1.8",
-	dependencies = { "nvim-lua/plenary.nvim" },
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		{
+			"nvim-telescope/telescope-fzf-native.nvim",
+			build = "make",
+		},
+	},
 	config = function()
+		local telescope = require("telescope")
+		telescope.setup({})
+		if not pcall(telescope.load_extension, "fzf") then
+			vim.notify(
+				"telescope-fzf-native.nvim not built; run :Lazy build telescope-fzf-native.nvim",
+				vim.log.levels.WARN
+			)
+		end
+
 		local builtin = require("telescope.builtin")
-		vim.keymap.set("n", "<leader>f", builtin.find_files, { desc = "Telescope find files" })
-		vim.keymap.set("n", "<leader>g", builtin.live_grep, { desc = "Telescope live grep" })
-		vim.keymap.set("n", "<leader>b", builtin.buffers, { desc = "Telescope buffers" })
+		vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
+		vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
+		vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
 		vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
 	end,
 }
