a565292b2cbde4d06cc301da63c3d1dcad6a92ff
Jonah Jacobsen <jjacobsen115@gmail.com>
2025-11-06 10:23:36 -0600

    Hide providers in lua config

--- init.lua
+++ init.lua
@@ -4,4 +4,9 @@ vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.termguicolors = true
 
+vim.g.loaded_node_provider = 0
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_python3_provider = 0
+vim.g.loaded_ruby_provider = 0
+
 require("config.lazy")
