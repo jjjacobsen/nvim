5f5a856b59542ca65f30a91a0a20d504d2617d6e
Jonah Jacobsen <jjacobsen115@gmail.com>
2025-11-06 16:24:16 -0600

    Add hjson support for comment

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -1,2 +1,13 @@
-return { 'numToStr/Comment.nvim' }
+return {
+	"numToStr/Comment.nvim",
+	config = function()
+		require("Comment").setup()
 
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = "hjson",
+			callback = function()
+				vim.bo.commentstring = "# %s"
+			end,
+		})
+	end,
+}
