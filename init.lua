59bef1fbb19d8560fc8ff8434d8ead42f9525ccc
Jonah Jacobsen <jjacobsen115@gmail.com>
2025-11-08 17:23:53 -0600

    Add keybindings for copying filename and file path

--- init.lua
+++ init.lua
@@ -10,3 +10,13 @@ vim.g.loaded_python3_provider = 0
 vim.g.loaded_ruby_provider = 0
 
 require("config.lazy")
+
+vim.keymap.set("n", "<leader>cp", function()
+	vim.fn.setreg("+", vim.fn.expand("%:p"))
+	print("Copied path: " .. vim.fn.expand("%:p"))
+end, { desc = "Copy full file path to clipboard" })
+
+vim.keymap.set("n", "<leader>cf", function()
+	vim.fn.setreg("+", vim.fn.expand("%:t"))
+	print("Copied filename: " .. vim.fn.expand("%:t"))
+end, { desc = "Copy filename to clipboard" })
