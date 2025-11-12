08090e51ae9d3180c46e1c8dddf49f5868e9baf0
Jonah Jacobsen <jjacobsen115@gmail.com>
2025-11-11 19:15:36 -0600

    Change default tab settings from 8 to 2

--- init.lua
+++ init.lua
@@ -4,6 +4,11 @@ vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.termguicolors = true
 
+vim.opt.expandtab = true
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.softtabstop = 2
+
 vim.g.loaded_node_provider = 0
 vim.g.loaded_perl_provider = 0
 vim.g.loaded_python3_provider = 0
