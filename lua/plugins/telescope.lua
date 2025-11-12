933f7650cb84ff89b0d23fa6fd667725f2582c32
Jonah Jacobsen <jjacobsen115@gmail.com>
2025-11-11 18:57:35 -0600

    Add keybindings for telescope to search hidden files and grep hidden files

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -36,7 +36,17 @@ return {
 
 		local builtin = require("telescope.builtin")
 		vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
+		vim.keymap.set("n", "<leader>fa", function()
+			builtin.find_files({ hidden = true, no_ignore = true })
+		end, { desc = "Telescope find all files" })
 		vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
+		vim.keymap.set("n", "<leader>ga", function()
+			builtin.live_grep({
+				additional_args = function()
+					return { "--hidden", "--no-ignore" }
+				end,
+			})
+		end, { desc = "Telescope live grep all" })
 		vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
 		vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
 		vim.keymap.set("n", "<leader>`", "<cmd>b#<cr>", { desc = "Switch to alternate buffer" })
