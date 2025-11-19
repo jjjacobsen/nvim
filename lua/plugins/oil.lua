de40e03746c99d79f01e4e2e449afe88f2f55327
Jonah Jacobsen <jjacobsen115@gmail.com>
2025-11-19 17:52:45 -0600

    Prevent cursor from switching to a block after confirmation

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -7,6 +7,17 @@ return {
 			["."] = "actions.toggle_hidden",
 		},
 	},
+	config = function(_, opts)
+		local util = require("oil.util")
+		local original_hide_cursor = util.hide_cursor
+		util.hide_cursor = function()
+			if vim.go.guicursor == "" then
+				return function() end
+			end
+			return original_hide_cursor()
+		end
+		require("oil").setup(opts)
+	end,
 	-- Optional dependencies
 	-- dependencies = { { "nvim-mini/mini.icons", opts = {} } },
 	dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
