7ea22cfc2404b22263a0618275b5715140ca1006
Jonah Jacobsen <jjacobsen115@gmail.com>
2026-01-30 15:40:11 -0600

    Update lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,13 +1,12 @@
--- return {
--- 	"nvim-lualine/lualine.nvim",
--- 	dependencies = { "nvim-tree/nvim-web-devicons" },
--- 	opts = {
--- 		sections = {
--- 			lualine_c = {
--- 				{ "filename", path = 1 },
--- 			},
--- 			lualine_x = { "filetype" },
--- 		},
--- 	},
--- }
-return {}
+return {
+	"nvim-lualine/lualine.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	opts = {
+		sections = {
+			lualine_c = {
+				{ "filename", path = 1 },
+			},
+			lualine_x = { "filetype" },
+		},
+	},
+}
