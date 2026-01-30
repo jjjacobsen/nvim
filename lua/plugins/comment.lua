a2c4b7f591f00393423b5643b1888201ae1b9ec3
Jonah Jacobsen <jjacobsen115@gmail.com>
2026-01-30 15:00:19 -0600

    Update comment.nvim

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -1,14 +1,1 @@
--- return {
--- 	"numToStr/Comment.nvim",
--- 	config = function()
--- 		require("Comment").setup()
-
--- 		vim.api.nvim_create_autocmd("FileType", {
--- 			pattern = "hjson",
--- 			callback = function()
--- 				vim.bo.commentstring = "# %s"
--- 			end,
--- 		})
--- 	end,
--- }
-return {}
+return { "numToStr/Comment.nvim" }
