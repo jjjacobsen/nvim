25b849168c63dca4fe4a85c444f33e2a2df89d58
Jonah Jacobsen <jjacobsen115@gmail.com>
2026-01-30 15:30:33 -0600

    Update tokyonight.nvim

--- lua/plugins/tokyonight.lua
+++ lua/plugins/tokyonight.lua
@@ -1,26 +1,25 @@
--- return {
--- 	"folke/tokyonight.nvim",
--- 	lazy = false,
--- 	priority = 1000,
--- 	opts = {
--- 		style = "night",
--- 		transparent = true,
--- 		styles = {
--- 			sidebars = "transparent",
--- 			floats = "transparent",
--- 		},
--- 		on_highlights = function(hl, c)
--- 			hl.Comment = { fg = c.blue1, italic = true }
--- 			hl["@comment"] = hl.Comment
--- 			hl.LineNr = { fg = c.fg }
--- 			hl.LineNrAbove = { fg = c.fg }
--- 			hl.LineNrBelow = { fg = c.fg }
--- 			hl.CursorLineNr = { fg = c.blue, bold = true }
--- 		end,
--- 	},
--- 	config = function(_, opts)
--- 		require("tokyonight").setup(opts)
--- 		vim.cmd.colorscheme("tokyonight")
--- 	end,
--- }
-return {}
+return {
+	"folke/tokyonight.nvim",
+	lazy = false,
+	priority = 1000,
+	opts = {
+		style = "night",
+		transparent = true,
+		styles = {
+			sidebars = "transparent",
+			floats = "transparent",
+		},
+		on_highlights = function(hl, c)
+			hl.Comment = { fg = c.blue1, italic = true }
+			hl["@comment"] = hl.Comment
+			hl.LineNr = { fg = c.fg }
+			hl.LineNrAbove = { fg = c.fg }
+			hl.LineNrBelow = { fg = c.fg }
+			hl.CursorLineNr = { fg = c.blue, bold = true }
+		end,
+	},
+	config = function(_, opts)
+		require("tokyonight").setup(opts)
+		vim.cmd.colorscheme("tokyonight")
+	end,
+}
