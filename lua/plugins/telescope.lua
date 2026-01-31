f39c6d0e31697dbeaf3c80d127face6ab9825764
Jonah Jacobsen <jjacobsen115@gmail.com>
2026-01-31 16:18:06 -0600

    Switch path_display in telescope to truncate

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -14,7 +14,7 @@ return {
 
 		telescope.setup({
 			defaults = {
-				path_display = { "smart" },
+				path_display = { "truncate" },
 				mappings = {
 					i = {
 						["<C-j>"] = actions.move_selection_next,
