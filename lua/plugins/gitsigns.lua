0281e3acb863d4214cb2767247eebadff05f48b1
Jonah Jacobsen <jjacobsen115@gmail.com>
2026-01-30 18:04:44 -0600

    Update gitsigns and add blame keybindings

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,2 +1,9 @@
--- return { "lewis6991/gitsigns.nvim", opts = {} }
-return {}
+return {
+	"lewis6991/gitsigns.nvim",
+	lazy = false,
+	keys = {
+		{ "gb", "<cmd>Gitsigns blame<cr>", desc = "Gitsigns blame" },
+		{ "gl", "<cmd>Gitsigns blame_line<cr>", desc = "Gitsigns blame line" },
+	},
+	opts = {},
+}
