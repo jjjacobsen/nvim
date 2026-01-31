ba0854c15ad61605efe816de96342a5d505e3cc9
Jonah Jacobsen <jjacobsen115@gmail.com>
2026-01-31 00:03:30 -0600

    Run stylua . to format lua correctly

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,7 +1,7 @@
 return {
-  "neovim/nvim-lspconfig",
-  config = function()
-    -- Neovim 0.11+ recommended pattern
-    vim.lsp.enable("pyright")
-  end,
+	"neovim/nvim-lspconfig",
+	config = function()
+		-- Neovim 0.11+ recommended pattern
+		vim.lsp.enable("pyright")
+	end,
 }
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,6 +1,6 @@
 return {
 	"nvim-telescope/telescope.nvim",
-    version = "*",
+	version = "*",
 	dependencies = {
 		"nvim-lua/plenary.nvim",
 		{
