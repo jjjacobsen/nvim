1c50577ca67ab421e3c872e8c92831f88e679927
Jonah Jacobsen <jjacobsen115@gmail.com>
2026-02-17 15:35:32 -0600

    Patch to fix auto-session and dashboard bug

--- lua/plugins/auto-session.lua
+++ lua/plugins/auto-session.lua
@@ -7,6 +7,26 @@ return {
 	---@type AutoSession.Config
 	opts = {
 		suppressed_dirs = { "~/", "~/Projects", "~/Downloads", "/" },
+		post_restore_cmds = {
+			function()
+				local buf = vim.api.nvim_get_current_buf()
+				local filetype = vim.bo[buf].filetype
+				local first_line = vim.api.nvim_buf_get_lines(buf, 0, 1, false)[1]
+				local name = vim.api.nvim_buf_get_name(buf)
+				local should_open_dashboard = name == ""
+					and (filetype == "" or filetype == "dashboard")
+					and first_line == ""
+				if should_open_dashboard then
+					vim.schedule(function()
+						if vim.api.nvim_buf_is_valid(buf) then
+							vim.bo[buf].modifiable = true
+						end
+						require("lazy").load({ plugins = { "dashboard-nvim" } })
+						vim.cmd("Dashboard")
+					end)
+				end
+			end,
+		},
 		-- log_level = 'debug',
 	},
 }
