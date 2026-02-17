88378d0baeddf8aeebd815c006455d0a7da0a376
Jonah Jacobsen <jjacobsen115@gmail.com>
2026-02-17 09:12:40 -0600

    Add better options to flash

--- lua/plugins/flash.lua
+++ lua/plugins/flash.lua
@@ -2,7 +2,16 @@ return {
 	"folke/flash.nvim",
 	event = "VeryLazy",
 	---@type Flash.Config
-	opts = {},
+	opts = {
+		modes = {
+			search = {
+				enabled = true,
+			},
+			char = {
+				jump_labels = true,
+			},
+		},
+	},
 	keys = {
 		{
 			"s",
