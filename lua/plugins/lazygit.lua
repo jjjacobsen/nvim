95c5f11bebc92303ef99f43728bfd75ae70aa502
Jonah Jacobsen <jjacobsen115@gmail.com>
2026-03-25 23:17:49 -0500

    Add double-Esc quit mapping for lazygit

--- lua/plugins/lazygit.lua
+++ lua/plugins/lazygit.lua
@@ -12,6 +12,14 @@ return {
 	dependencies = {
 		"nvim-lua/plenary.nvim",
 	},
+	config = function()
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = "lazygit",
+			callback = function(event)
+				vim.keymap.set("t", "<Esc><Esc>", "q", { buffer = event.buf, silent = true })
+			end,
+		})
+	end,
 	-- setting the keybinding for LazyGit with 'keys' is recommended in
 	-- order to load the plugin when the command is run for the first time
 	keys = {
