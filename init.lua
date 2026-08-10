173297c95efd65451e8a62b288aafadf44e82179
Jonah Jacobsen <jjacobsen115@gmail.com>
2026-08-10 09:39:21 -0500

    undofile = true

--- init.lua
+++ init.lua
@@ -3,6 +3,7 @@ vim.opt.clipboard = "unnamedplus"
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.termguicolors = true
+vim.opt.undofile = true
 vim.opt.sessionoptions = {
 	"blank",
 	"buffers",
